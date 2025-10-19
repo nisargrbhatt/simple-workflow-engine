b191df82e02337121918271d45e682f8f284adab
Nisarg Bhatt <nisargrbhatt@gmail.com>
2025-10-19 14:41:44 +0530

    Guard task process added

--- packages/engine/src/engine/index.ts
+++ packages/engine/src/engine/index.ts
@@ -1,16 +1,17 @@
-import type { ProcessorResult, RuntimeInfo } from '../types/index';
-import type { WorkflowPersistor } from '../persistor/index';
-import { LogPersistor } from '../persistor/log';
-import { StartProcessor } from './processors/start';
-import { safeAsync } from '@repo/utils';
-import { EndProcessor } from './processors/end';
-import { FunctionProcessor } from './processors/function';
+import type { ProcessorResult, RuntimeInfo } from "../types/index";
+import type { WorkflowPersistor } from "../persistor/index";
+import { LogPersistor } from "../persistor/log";
+import { StartProcessor } from "./processors/start";
+import { safeAsync } from "@repo/utils";
+import { EndProcessor } from "./processors/end";
+import { FunctionProcessor } from "./processors/function";
+import { GuardProcessor } from "./processors/guard";
 
 export class Engine {
   runtime: RuntimeInfo | null = null;
 
   constructor(public db: WorkflowPersistor) {
-    console.log('Engine initialized');
+    console.log("Engine initialized");
   }
 
   async setup(runtimeId: number) {
@@ -20,18 +21,22 @@ export class Engine {
 
   async process(taskId: string) {
     if (!this.runtime) {
-      throw new Error('No runtime found. Please call setup first');
+      throw new Error("No runtime found. Please call setup first");
     }
 
     const task = this.runtime?.tasks?.find((t) => t.id === taskId);
     if (!task) {
-      throw new Error('Task not found');
+      throw new Error("Task not found");
     }
 
-    const logger = new LogPersistor(this.runtime?.id, this.runtime?.definitionId, task.id);
+    const logger = new LogPersistor(
+      this.runtime?.id,
+      this.runtime?.definitionId,
+      task.id
+    );
 
-    if (task.status === 'completed' || task.status === 'failed') {
-      throw new Error('Task is already processed');
+    if (task.status === "completed" || task.status === "failed") {
+      throw new Error("Task is already processed");
     }
 
     const taskResultMap = new Map<string, any>();
@@ -40,11 +45,11 @@ export class Engine {
     });
     const taskResults = Object.fromEntries(taskResultMap);
 
-    await this.db.updateRuntimeTaskStatus(this.runtime.id, task.id, 'pending');
+    await this.db.updateRuntimeTaskStatus(this.runtime.id, task.id, "pending");
 
     let result: Awaited<ReturnType<typeof safeAsync<ProcessorResult>>>;
 
-    if (task.type === 'START') {
+    if (task.type === "START") {
       const processor = new StartProcessor({
         task,
         global: this.runtime?.global,
@@ -53,8 +58,8 @@ export class Engine {
       });
 
       result = await safeAsync(processor.process());
-      await this.db.updateRuntimeStatus(this.runtime.id, 'pending');
-    } else if (task.type === 'END') {
+      await this.db.updateRuntimeStatus(this.runtime.id, "pending");
+    } else if (task.type === "END") {
       const processor = new EndProcessor({
         task,
         global: this.runtime?.global,
@@ -63,8 +68,8 @@ export class Engine {
       });
 
       result = await safeAsync(processor.process());
-      await this.db.updateRuntimeStatus(this.runtime.id, 'completed');
-    } else if (task.type === 'FUNCTION') {
+      await this.db.updateRuntimeStatus(this.runtime.id, "completed");
+    } else if (task.type === "FUNCTION") {
       const processor = new FunctionProcessor({
         task,
         global: this.runtime?.global,
@@ -73,16 +78,33 @@ export class Engine {
       });
 
       result = await safeAsync(processor.process());
+    } else if (task.type === "GUARD") {
+      const processor = new GuardProcessor({
+        task,
+        global: this.runtime?.global,
+        taskResults,
+        logger,
+      });
+
+      result = await safeAsync(processor.process());
     } else {
-      throw new Error('Invalid task type');
+      throw new Error("Invalid task type");
     }
 
     if (result.success) {
-      await this.db.updateRuntimeTaskResult(this.runtime.id, task.id, result.data.result);
-      await this.db.updateRuntimeTaskStatus(this.runtime.id, task.id, 'completed');
+      await this.db.updateRuntimeTaskResult(
+        this.runtime.id,
+        task.id,
+        result.data.result
+      );
+      await this.db.updateRuntimeTaskStatus(
+        this.runtime.id,
+        task.id,
+        "completed"
+      );
     } else {
       console.error(result.error);
-      await this.db.updateRuntimeTaskStatus(this.runtime.id, task.id, 'failed');
+      await this.db.updateRuntimeTaskStatus(this.runtime.id, task.id, "failed");
     }
 
     if (logger.Logs.length > 0) {
@@ -91,14 +113,20 @@ export class Engine {
 
     // Call Next Task Block
     if (result.success) {
-      if (task.type === 'START') {
+      if (task.type === "START") {
         const nextTasks = task.next;
 
         this._callNextTasks(nextTasks);
-      } else if (task.type === 'FUNCTION') {
+      } else if (task.type === "FUNCTION") {
         const nextTasks = task.next;
 
         this._callNextTasks(nextTasks);
+      } else if (task.type === "GUARD") {
+        if (result.data.result === true) {
+          const nextTasks = task.next;
+
+          this._callNextTasks(nextTasks);
+        }
       }
     }
   }
@@ -109,7 +137,7 @@ export class Engine {
     }
 
     if (!this.runtime) {
-      throw new Error('No runtime found. Please call setup first');
+      throw new Error("No runtime found. Please call setup first");
     }
 
     const runtimeId = this.runtime.id;
